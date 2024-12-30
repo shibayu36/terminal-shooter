24dee1c01f1ff349247478dacb2ff24a5ba14967
shibayu36 <shibayu36@gmail.com>
2024-12-30 14:51:15 +0900

    ひとまずmqttサーバーの立ち上げまで

--- go.mod
+++ go.mod
@@ -2,14 +2,20 @@ module github.com/shibayu36/terminal-shooter
 
 go 1.23.2
 
-require github.com/gdamore/tcell/v2 v2.7.4
+require (
+	github.com/gdamore/tcell/v2 v2.7.4
+	github.com/mochi-mqtt/server/v2 v2.6.6
+)
 
 require (
 	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/rivo/uniseg v0.4.3 // indirect
-	golang.org/x/sys v0.17.0 // indirect
+	github.com/rs/xid v1.4.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/term v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ server/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	mqtt "github.com/mochi-mqtt/server/v2"
+	"github.com/mochi-mqtt/server/v2/hooks/auth"
+	"github.com/mochi-mqtt/server/v2/listeners"
+)
+
+func main() {
+	// サーバーが中断されるまで実行するためのシグナルチャネルを作成
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-sigs
+		done <- true
+	}()
+
+	// MQTTサーバーの作成
+	server := mqtt.New(&mqtt.Options{})
+	// 今回は認証なし
+	_ = server.AddHook(new(auth.AllowHook), nil)
+
+	// TCPリスナーの追加
+	tcp := listeners.NewTCP(listeners.Config{ID: "t1", Address: ":1883"})
+	err := server.AddListener(tcp)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// サーバーの起動
+	go func() {
+		err := server.Serve()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	// サーバーが中断されるまで実行
+	<-done
+
+	server.Log.Warn("caught signal, stopping...")
+	_ = server.Close()
+	server.Log.Info("main.go finished")
+}
