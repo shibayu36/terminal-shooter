e6aa5aed847b56bbd5076cc18e08bf8ddf8d9cd6
shibayu36 <shibayu36@gmail.com>
2024-12-30 15:13:42 +0900

    ひとまず何らかのハンドラーを追加するところまで

--- server/main.go
+++ server/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
@@ -9,6 +11,7 @@ import (
 	mqtt "github.com/mochi-mqtt/server/v2"
 	"github.com/mochi-mqtt/server/v2/hooks/auth"
 	"github.com/mochi-mqtt/server/v2/listeners"
+	"github.com/mochi-mqtt/server/v2/packets"
 )
 
 func main() {
@@ -33,6 +36,14 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// サーバーにフックを追加
+	err = server.AddHook(new(Hook), &HookOptions{
+		Server: server,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	// サーバーの起動
 	go func() {
 		err := server.Serve()
@@ -47,4 +58,77 @@ func main() {
 	server.Log.Warn("caught signal, stopping...")
 	_ = server.Close()
 	server.Log.Info("main.go finished")
+}
+
+type HookOptions struct {
+	Server *mqtt.Server
+}
+
+type Hook struct {
+	mqtt.HookBase
+	config *HookOptions
+}
+
+func (h *Hook) ID() string {
+	return "events-example"
+}
+
+func (h *Hook) Provides(b byte) bool {
+	return bytes.Contains([]byte{
+		mqtt.OnConnect,
+		mqtt.OnDisconnect,
+		mqtt.OnPublished,
+		mqtt.OnPublish,
+	}, []byte{b})
+}
+
+func (h *Hook) Init(config any) error {
+	h.Log.Info("initialised")
+	if _, ok := config.(*HookOptions); !ok && config != nil {
+		return mqtt.ErrInvalidConfigType
+	}
+
+	h.config = config.(*HookOptions)
+	if h.config.Server == nil {
+		return mqtt.ErrInvalidConfigType
+	}
+	return nil
+}
+
+func (h *Hook) OnConnect(cl *mqtt.Client, pk packets.Packet) error {
+	h.Log.Info("client connected", "client", cl.ID)
+	return nil
+}
+
+func (h *Hook) OnDisconnect(cl *mqtt.Client, err error, expire bool) {
+	if err != nil {
+		h.Log.Info("client disconnected", "client", cl.ID, "expire", expire, "error", err)
+	} else {
+		h.Log.Info("client disconnected", "client", cl.ID, "expire", expire)
+	}
+
+}
+
+func (h *Hook) OnSubscribed(cl *mqtt.Client, pk packets.Packet, reasonCodes []byte) {
+	h.Log.Info(fmt.Sprintf("subscribed qos=%v", reasonCodes), "client", cl.ID, "filters", pk.Filters)
+}
+
+func (h *Hook) OnUnsubscribed(cl *mqtt.Client, pk packets.Packet) {
+	h.Log.Info("unsubscribed", "client", cl.ID, "filters", pk.Filters)
+}
+
+func (h *Hook) OnPublish(cl *mqtt.Client, pk packets.Packet) (packets.Packet, error) {
+	h.Log.Info("received from client", "client", cl.ID, "payload", string(pk.Payload))
+
+	pkx := pk
+	if string(pk.Payload) == "hello" {
+		pkx.Payload = []byte("hello world")
+		h.Log.Info("received modified packet from client", "client", cl.ID, "payload", string(pkx.Payload))
+	}
+
+	return pkx, nil
+}
+
+func (h *Hook) OnPublished(cl *mqtt.Client, pk packets.Packet) {
+	h.Log.Info("published to client", "client", cl.ID, "payload", string(pk.Payload))
 }
