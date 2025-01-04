b31bb05039f83dc36c0f3ae962564eb5912abb4c
shibayu36 <shibayu36@gmail.com>
2025-01-04 13:42:55 +0900

    弾の移動を表現する

--- server/state_test.go
+++ server/state_test.go
@@ -33,3 +33,23 @@ func Test_GameState(t *testing.T) {
 	assert.Len(t, gameState.GetPlayers(), 1)
 	assert.Equal(t, 0, gameState.GetPlayers()["player2"].Position.X)
 }
+
+func Test_Bullet(t *testing.T) {
+	bullet := NewBullet("bullet1", &Position{X: 3, Y: 8}, DirectionRight)
+
+	assert.Equal(t, ItemID("bullet1"), bullet.ID())
+	assert.Equal(t, ItemTypeBullet, bullet.Type())
+	assert.Equal(t, &Position{X: 3, Y: 8}, bullet.Position())
+
+	// 1回目は更新されない
+	assert.False(t, bullet.Update())
+
+	// その後29回目まで更新されない
+	for i := 1; i < 29; i++ {
+		assert.False(t, bullet.Update())
+	}
+
+	// 30回目は更新される
+	assert.True(t, bullet.Update())
+	assert.Equal(t, &Position{X: 4, Y: 8}, bullet.Position())
+}
