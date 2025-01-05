9e878e75822769245e84484f38f5cd4f37341bb8
shibayu36 <shibayu36@gmail.com>
2025-01-05 17:31:12 +0900

    プレイヤーの前方座標を取得する

--- server/game/player.go
+++ server/game/player.go
@@ -9,6 +9,12 @@ type Player struct {
 	Direction Direction
 }
 
+// プレイヤーの前方の座標を取得する
+func (p *Player) FowardPosition() Position {
+	dx, dy := p.Direction.ToVector()
+	return Position{X: p.Position.X + dx, Y: p.Position.Y + dy}
+}
+
 // プレイヤーの状態をshared.PlayerStateに変換する
 func (ps *Player) ToSharedPlayerState(status shared.Status) *shared.PlayerState {
 	return &shared.PlayerState{
---
+++ server/game/player_test.go
@@ -0,0 +1,25 @@
+package game
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Player_FowardPosition(t *testing.T) {
+	testcases := []struct {
+		position  Position
+		direction Direction
+		expected  Position
+	}{
+		{Position{X: 3, Y: 8}, DirectionRight, Position{X: 4, Y: 8}},
+		{Position{X: 1, Y: 2}, DirectionLeft, Position{X: 0, Y: 2}},
+		{Position{X: 5, Y: 6}, DirectionUp, Position{X: 5, Y: 5}},
+		{Position{X: 7, Y: 8}, DirectionDown, Position{X: 7, Y: 9}},
+	}
+
+	for _, tc := range testcases {
+		player := &Player{Position: tc.position, Direction: tc.direction}
+		assert.Equal(t, player.FowardPosition(), tc.expected)
+	}
+}
