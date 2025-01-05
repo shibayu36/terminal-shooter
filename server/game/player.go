f4790e70d90c8a8afa1235a1472df51e9ab6d4c3
shibayu36 <shibayu36@gmail.com>
2025-01-05 17:02:18 +0900

    player.goを分離

---
+++ server/game/player.go
@@ -0,0 +1,23 @@
+package game
+
+import "github.com/shibayu36/terminal-shooter/shared"
+
+// プレイヤーの状態を管理する
+type PlayerState struct {
+	PlayerID  PlayerID
+	Position  Position
+	Direction Direction
+}
+
+// プレイヤーの状態をshared.PlayerStateに変換する
+func (ps *PlayerState) ToSharedPlayerState(status shared.Status) *shared.PlayerState {
+	return &shared.PlayerState{
+		PlayerId: string(ps.PlayerID),
+		Position: &shared.Position{
+			X: int32(ps.Position.X),
+			Y: int32(ps.Position.Y),
+		},
+		Direction: ps.Direction.ToSharedDirection(),
+		Status:    status,
+	}
+}
