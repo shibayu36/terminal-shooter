d9fce5c26d129cecc203fdcbe95780663cd42d9f
shibayu36 <shibayu36@gmail.com>
2025-01-12 14:33:27 +0900

    MessageStats構造体をファイル移動

--- client/main.go
+++ client/main.go
@@ -31,38 +31,6 @@ type Item struct {
 	Position Position
 }
 
-type MessageStats struct {
-	count    int
-	lastTime time.Time
-	rate     float64
-}
-
-// メッセージを受信したことを記録する
-func (m *MessageStats) RecordMessage() {
-	m.count++
-}
-
-// メッセージレートを計算する
-func (m *MessageStats) Calculate() {
-	now := time.Now()
-	if m.lastTime.IsZero() {
-		m.lastTime = now
-		return
-	}
-
-	duration := now.Sub(m.lastTime).Seconds()
-	if duration >= 1.0 { // 1秒以上経過している場合に計算
-		m.rate = float64(m.count) / duration
-		m.count = 0
-		m.lastTime = now
-	}
-}
-
-// 現在のレートを取得する
-func (m *MessageStats) Rate() float64 {
-	return m.rate
-}
-
 type Game struct {
 	mqtt mqtt.Client
 
