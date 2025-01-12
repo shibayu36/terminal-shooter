d0cd552a635747c04ba1efb75b53dfb7f69e238f
shibayu36 <shibayu36@gmail.com>
2025-01-12 20:22:34 +0900

    ゲームループの実行時間をメトリクスに

--- server/stats/metrics.go
+++ server/stats/metrics.go
@@ -16,3 +16,14 @@ var PublishedPackets = promauto.NewCounter(prometheus.CounterOpts{
 	Name: "terminal_shooter_published_packets_total",
 	Help: "The total number of published packets",
 })
+
+// ゲーム更新ループの実行時間
+var GameLoopDuration = promauto.NewHistogram(prometheus.HistogramOpts{
+	Name: "terminal_shooter_game_loop_duration_seconds",
+	Help: "Time spent processing game loop",
+	Buckets: []float64{
+		0.0001, 0.001, 0.005, 0.01, 0.015,
+		0.016667,
+		0.02, 0.025, 0.03,
+	},
+})
