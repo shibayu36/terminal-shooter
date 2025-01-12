8a9ede567a4e9bd440b8ed2020252db638fd1fd1
shibayu36 <shibayu36@gmail.com>
2025-01-12 16:49:43 +0900

    Publishされたパケット数を測定

--- server/stats/metrics.go
+++ server/stats/metrics.go
@@ -10,3 +10,9 @@ var ActiveClients = promauto.NewGauge(prometheus.GaugeOpts{
 	Name: "terminal_shooter_active_clients",
 	Help: "The number of active clients",
 })
+
+// Publishされたパケット数
+var PublishedPackets = promauto.NewCounter(prometheus.CounterOpts{
+	Name: "terminal_shooter_published_packets_total",
+	Help: "The total number of published packets",
+})
